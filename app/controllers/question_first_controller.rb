c613bbdd45d85254e17cbb02e49b56403db94289
梅木健 <umeki@MacBook-Air.local>
2025-03-11 16:59:11 +0900

    ページネーションとセッションの処理

--- app/controllers/question_first_controller.rb
+++ app/controllers/question_first_controller.rb
@@ -1,40 +1,44 @@
 class QuestionFirstController < ApplicationController
-    def index
-        @questions = Question.where(part: 1)
+  def index
+    @questions = Question.where(part: 1)
+  end
+
+  def create
+    # 全ての質問が回答されているかチェック
+    if params[:answers].present? && params[:answers].keys.count == Question.where(part: 1).count
+      @total_score = params[:answers].values.map(&:to_i).sum
+
+      # 回答を保存
+      params[:answers].each do |question_id, score|
+        UserAnswer.create(
+          user_id: current_user.id,
+          question_id: question_id.to_i,
+          selected_option: score.to_i
+        )
+      end
+
+      # スコアによる分岐処理
+      if @total_score >= 7
+        dolphin_chronotype = Chronotype.find_by(name: "イルカ型")
+        UserChronotype.create(
+          user_id: current_user.id,
+          chronotype_id: dolphin_chronotype.id,
+          score: @total_score
+        )
+        redirect_to dolphin_path
+      else
+        redirect_to question_second_path
+      end
+    else
+      # 質問に回答していない場合はエラーメッセージを表示してリダイレクト
+      flash[:alert] = "全ての質問に回答してください。"
+      redirect_to question_first_index_path
     end
+  end
 
-    def create
-        @total_score = params[:answers].values.map(&:to_i).sum
+  private
 
-        params[:answers].each do |question_id, score|
-          UserAnswer.create(
-            user_id: current_user.id,
-            question_id: question_id.to_i,
-            selected_option: score.to_i
-          )
-        end
-
-        if @total_score >= 7
-            dolphin_chronotype = Chronotype.find_by(name: "イルカ型")
-            UserChronotype.create(
-            user_id: current_user.id,
-            chronotype_id: dolphin_chronotype.id,
-            score: @total_score
-            )
-            redirect_to dolphin_path
-        else
-          redirect_to question_second_path
-        end
-    end
-
-
-
-
-
-
-    private
-
-    def user_answer_params
-      params.require(:user_answer).permit(:question_id, :selected_option)
-    end
+  def user_answer_params
+    params.require(:user_answer).permit(:question_id, :selected_option)
+  end
 end
--- app/controllers/question_second_controller.rb
+++ app/controllers/question_second_controller.rb
@@ -1,47 +1,95 @@
 class QuestionSecondController < ApplicationController
-    def index
-        @questions = Question.where(part: 2)
+  def index
+    @questions = Question.where(part: 2).page(params[:page]).per(5)
+    @total_questions = Question.where(part: 2).count
+  end
+
+  def create
+    session[:answers] ||= {}
+    current_page = params[:page].to_i
+    questions_on_page = Question.where(part: 2).page(current_page).per(5)
+
+    if params[:answers].present? && params[:answers].keys.count == questions_on_page.count
+      session[:answers].merge!(filtered_answers)
+
+      if current_page * 5 >= Question.where(part: 2).count
+        process_all_answers
+      else
+        redirect_to question_second_path(page: current_page + 1)
+      end
+    else
+      flash[:alert] = "全ての質問に回答してください。"
+      redirect_to question_second_path(page: current_page)
+    end
+  end
+
+  private
+
+  def filtered_answers
+    params.require(:answers).permit!.to_h
+  rescue ActionController::ParameterMissing
+    {}
+  end
+
+  def process_all_answers
+    all_answers = session[:answers]
+
+    if all_answers.blank?
+      flash[:alert] = "質問に回答してください。"
+      redirect_to question_second_path(page: params[:page])
+      return
     end
 
-    def create
-        @total_score = params[:answers].values.map(&:to_i).sum
+    @total_score = all_answers.values.map(&:to_i).sum
 
-        params[:answers].each do |question_id, score|
-          UserAnswer.create(
-            user_id: current_user.id,
-            question_id: question_id.to_i,
-            selected_option: score.to_i
-          )
-        end
-
-        if @total_score >= 48
-            chronotype_name = "オオカミ型"
-            redirect_to wolf_path
-        elsif @total_score >= 34
-            chronotype_name = "クマ型"
-            redirect_to bear_path
-        elsif @total_score >= 20
-            chronotype_name = "ライオン型"
-            redirect_to lion_path
-        else
-          redirect_to question_second_path and return
-        end
-        chronotype = Chronotype.find_by(name: chronotype_name)
-         UserChronotype.create(
-         user_id: current_user.id,
-         chronotype_id: chronotype.id,
-         score: @total_score
-         )
+    all_answers.each do |question_id, score|
+      UserAnswer.create(
+        user_id: current_user.id,
+        question_id: question_id.to_i,
+        selected_option: score.to_i
+      )
     end
 
+    chronotype_name = determine_chronotype(@total_score)
 
-
-
-
-
-    private
-
-    def user_answer_params
-      params.require(:user_answer).permit(:question_id, :selected_option)
+    if chronotype_name.nil?
+      redirect_to question_second_path, alert: "スコアが足りません。" and return
     end
+
+    chronotype = Chronotype.find_by(name: chronotype_name)
+
+    if chronotype.nil?
+      Rails.logger.error "Chronotype not found for name: #{chronotype_name}"
+      redirect_to question_second_path, alert: "クロノタイプが見つかりませんでした。" and return
+    end
+
+    UserChronotype.create(
+      user_id: current_user.id,
+      chronotype_id: chronotype.id,
+      score: @total_score
+    )
+
+    session.delete(:answers)
+
+    redirect_to_chronotype_path(chronotype_name)
+  end
+
+  def determine_chronotype(total_score)
+    case total_score
+    when 48..Float::INFINITY then "オオカミ型"
+    when 34..47 then "クマ型"
+    when 20..33 then "ライオン型"
+    else nil
+    end
+  end
+
+  def redirect_to_chronotype_path(chronotype_name)
+    case chronotype_name
+    when "オオカミ型" then redirect_to wolf_path and return
+    when "クマ型" then redirect_to bear_path and return
+    when "ライオン型" then redirect_to lion_path and return
+    when "イルカ型" then redirect_to dolphin_path and return
+    else redirect_to question_second_path, alert: "クロノタイプが正しく判定できませんでした。"
+    end
+  end
 end
