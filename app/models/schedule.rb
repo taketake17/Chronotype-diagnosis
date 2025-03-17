3a8734b188660d631b76d431f91f3c01ade4cfc8
梅木健 <umeki@MacBook-Air.local>
2025-03-17 17:48:22 +0900

    修正

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,5 @@
 class Schedule < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: false
     validates :title, presence: true
     validates :start_time, presence: true
     validates :end_time, presence: true
