dc32caebfaf065308f1876932e9ff8fc7757b006
梅木健 <umeki@MacBook-Air.local>
2025-03-17 11:50:27 +0900

    ブロックをクリックできるようにする

--- app/javascript/calendar.js
+++ app/javascript/calendar.js
@@ -1,16 +1,23 @@
 import { Calendar } from '@fullcalendar/core';
+import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from '@fullcalendar/timegrid';
+
 
 document.addEventListener('DOMContentLoaded', function() {
   const calendarEl = document.getElementById('calendar');
   const calendar = new Calendar(calendarEl, {
-    plugins: [timeGridPlugin],
+    plugins: [interactionPlugin,timeGridPlugin],
     initialView: 'timeGridWeek',
     headerToolbar: {
       left: 'prev,next today',
       center: 'title',
       right: 'timeGridWeek,timeGridDay'
-    }
+    },
+
+    selectable: true,
+    select: function (info) {
+        alert("selected " + info.startStr + " to " + info.endStr);
+    },
   });
   calendar.render();
 });
