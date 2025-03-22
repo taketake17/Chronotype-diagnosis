fa72a5ca90d269357c309bb05b033df6434aa27e
梅木健 <umeki@MacBook-Air.local>
2025-03-22 16:37:33 +0900

    デフォルトスケジュール削除

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Myapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Singapore"  # あるいは適切なタイムゾーン
+    config.time_zone = "Tokyo"  # あるいは適切なタイムゾーン
     config.active_record.default_timezone = :local
 
     # config.eager_load_paths << Rails.root.join("extras")
