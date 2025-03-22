5581b79b6d31c9465d097cabba79878735000e4e
梅木健 <umeki@MacBook-Air.local>
2025-03-22 15:40:14 +0900

    タイトル作成

--- app/controllers/question_first_controller.rb
+++ app/controllers/question_first_controller.rb
@@ -6,11 +6,9 @@ class QuestionFirstController < ApplicationController
   end
 
   def create
-    # 全ての質問が回答されているかチェック
     if params[:answers].present? && params[:answers].keys.count == Question.where(part: 1).count
       @total_score = params[:answers].values.map(&:to_i).sum
 
-      # 回答を保存
       params[:answers].each do |question_id, score|
         UserAnswer.create(
           user_id: current_user.id,
@@ -19,7 +17,6 @@ class QuestionFirstController < ApplicationController
         )
       end
 
-      # スコアによる分岐処理
       if @total_score >= 7
         dolphin_chronotype = Chronotype.find_by(name: "イルカ型")
         UserChronotype.create(
@@ -32,7 +29,6 @@ class QuestionFirstController < ApplicationController
         redirect_to question_second_path
       end
     else
-      # 質問に回答していない場合はエラーメッセージを表示してリダイレクト
       flash[:alert] = "全ての質問に回答してください。"
       redirect_to question_first_index_path
     end
