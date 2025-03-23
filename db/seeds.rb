c58a2e3799e94f3374eda3a5d2fa60f08fa306bc
梅木健 <umeki@MacBook-Air.local>
2025-03-23 14:36:49 +0900

    時間跨ぐのを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,7 @@
   { title: "集中モード", start_time: '07:00', end_time: '09:00' },
   { title: "運動", start_time: '09:00', end_time: '10:00' },
   { title: "夕食", start_time: '11:00', end_time: '11:30' },
-  { title: "就寝", start_time: '14:30', end_time: '15:00' },
-  { title: "睡眠", start_time: '15:00', end_time: '23:59' }
+  { title: "就寝", start_time: '14:30', end_time: '15:00' }
 ].each do |schedule|
   DefaultSchedule.create!(
     chronotype_id: 4,
@@ -40,8 +39,7 @@ end
   { title: "昼食", start_time: '03:00', end_time: '04:00' },
   { title: "集中モード", start_time: '07:00', end_time: '09:00' },
   { title: "夕食", start_time: '10:00', end_time: '11:00' },
-  { title: "就寝・睡眠", start_time: '14:30', end_time: '15:00' },
-  { title: "睡眠", start_time: '15:00', end_time: '23:59' }
+  { title: "就寝・睡眠", start_time: '14:30', end_time: '15:00' }
 ].each do |schedule|
   DefaultSchedule.create!(
     chronotype_id: 1,
@@ -63,8 +61,7 @@ end
   { title: "集中モード", start_time: '04:00', end_time: '06:00' },
   { title: "運動", start_time: '08:00', end_time: '09:00' },
   { title: "夕食", start_time: '09:00', end_time: '10:00' },
-  { title: "就寝", start_time: '13:00', end_time: '13:30' },
-  { title: "睡眠", start_time: '13:30', end_time: '23:59' }
+  { title: "就寝", start_time: '13:00', end_time: '13:30' }
 ].each do |schedule|
   DefaultSchedule.create!(
     chronotype_id: 2,
@@ -86,8 +83,7 @@ end
   { title: "昼寝", start_time: '05:00', end_time: '05:30' },
   { title: "運動", start_time: '09:00', end_time: '10:00' },
   { title: "夕食", start_time: '10:30', end_time: '11:00' },
-  { title: "就寝", start_time: '13:30', end_time: '14:00' },
-  { title: "睡眠", start_time: '14:00', end_time: '23:59' }
+  { title: "就寝", start_time: '13:30', end_time: '14:00' }
 ].each do |schedule|
   DefaultSchedule.create!(
     chronotype_id: 3,
