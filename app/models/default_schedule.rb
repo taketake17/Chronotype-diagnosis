d177df77fd1c430bc830582698f6098fe7412955
梅木健 <umeki@MacBook-Air.local>
2025-03-23 16:32:59 +0900

    seedの修正

--- app/models/default_schedule.rb
+++ app/models/default_schedule.rb
@@ -1,8 +1,17 @@
 class DefaultSchedule < ApplicationRecord
   belongs_to :chronotype
+
   def self.create_default_weekly_schedule(start_date, end_date, default_schedules)
     (start_date..end_date).flat_map do |date|
       default_schedules.map do |schedule|
+        start_time = combine_date_and_time(date, schedule.start_time)
+        end_time = combine_date_and_time(date, schedule.end_time)
+
+        # 日付をまたぐ場合の処理
+        if end_time < start_time
+          end_time = end_time + 1.day
+        end
+
         {
           id: "default_#{schedule.id}_#{date}",
           title: schedule.title,
