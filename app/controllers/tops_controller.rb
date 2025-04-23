4ead78d8dce4d925c28e2c1514910f1a6466ccd2
梅木健 <umeki@MacBook-Air.local>
2025-04-23 16:39:00 +0900

    meta

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,8 @@
 class TopsController < ApplicationController
     def index
       session.delete(:answers) if session[:answers].present? # answerセッションが存在する場合に削除
+      respond_to do |format|
+        format.html # これが必要
+      end
     end
 end
